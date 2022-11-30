e0d9d86c566d4429845936c6ad87c6be5eb6f48a
Rafael Alberto Larios <albertolarios@me.com>
2022-11-30 15:08:48 -0600

    Give tab buttons functionality and style

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import "./style.css";
-import * as home from "./home.js";
+import * as home from "./home";
 
 const container = document.querySelector("div#container");
-const homeBtn = document.querySelector("button#home");
-container.append(home.genContent());
+const btns = document.querySelectorAll("footer button");
 
-homeBtn.classList.add("active")
+const makeBtnsInactive = function() {
+    btns.forEach(x => x.classList.remove("active"));
+};
+
+const clearContainer = function() {
+    container.innerHTML = "";
+};
+
+const homeAction = function() {
+    makeBtnsInactive();
+    this.classList.add("active");
+    clearContainer();
+    container.append(home.genContent());
+};
+
+const homeBtn = document.querySelector("button#home");
+homeBtn.onclick = homeAction;
+
+const menuBtn = document.querySelector("button#menu");
+menuBtn.onclick = homeAction;
+
+const contactBtn = document.querySelector("button#contact");
+contactBtn.onclick = homeAction;
